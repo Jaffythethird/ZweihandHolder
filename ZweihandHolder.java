da74194ad2fc35ba513848122bf88766e6481559
Jordan Fry <jaffythethird@gmail.com>
2020-02-26 23:32:07 -0500

    added RNG

--- ZweihandHolder.java
+++ ZweihandHolder.java
@@ -1,7 +1,18 @@
+import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ZweihandHolder
 {
     public static void main(String[] args)
     {
         System.out.println("Yeah, this isn't done yet.");
+	System.out.println(Math.PI);
+    }
+
+    
+
+    public int roll(int size)
+    {
+	    return ThreadLocalRandom.current().nextInt(1,size + 1);
     }
 }
